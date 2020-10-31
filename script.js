50c83953b2856ba647f7cafaebc206f6905c3b73
mfrisch87 <mikey.frisch@gmail.com>
2020-10-31 13:32:07 -0700

    Still figuring out date picker

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 var dateInputEl = $('#datepicker');
 
+
+var printSkills = function (name, date) {
+    var listEl = $('<li>');
+    var listDetail = name.concat(' on ', date);
+    listEl.addClass('list-group-item').text(listDetail);
+  };
+
+
+
 $( function() {
     $( "#datepicker" ).datepicker();
   } );
